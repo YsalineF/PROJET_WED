5d56f53d16d08295f4b4d1af6f1a3160cc40c409
Mézz <ysaline.franquet@gmail.com>
2020-09-07 09:33:29 +0200

    Structure MVC Vierge

---
+++ app/vues/templates/index.php
@@ -0,0 +1,17 @@
+<?php
+/*
+  ./app/vues/templates/index.php
+  TEMPLATE PRINCIPAL
+*/
+ ?>
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title></title>
+   </head>
+   <body>
+     <h1>Template principal</h1>
+     <?php var_dump($connexion); ?>
+   </body>
+ </html>
---
+++ www/index.php
@@ -0,0 +1,11 @@
+<?php
+/*
+  ./www/index.php
+  DISPATCHER CENTRAL
+*/
+
+require_once '../noyau/init.php';
+
+require_once '../app/routeur.php';
+
+require_once '../app/vues/templates/index.php';
